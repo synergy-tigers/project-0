6f057cc61c55ba8936845e7ab4a41b92e4027067
Rhys <lev.k.kim@gmail.com>
2022-03-27 17:36:17 -0400

    did some black magic in the javascript

--- scripts.js
+++ scripts.js
@@ -29,36 +29,51 @@ async function getContent() {
 
 // now to grab the data from getcontent and display it for the whole world to see
 function displayData(obj) {
-  const body = document.querySelector("spellCard");
-
   // print the properties in the resulting object from api
   for (const key in obj) {
     if (Object.hasOwnProperty.call(obj, key)) {
       const element = obj[key];
 
+      // first, set up the data in html
+      const body = document.getElementById("spellCard");
+      const fields = document.getElementById("spellFields");
+      const table = document.getElementById("spellData");
+      let row = document.createElement("tr");
+      let cell = document.createElement("td");
+      let title = document.createElement("h2");
+      let field = document.createElement("p");
+
+      // loop through the keys and print them out in headers
+      body.appendChild(title);
+      title.innerHTML = key;
+
       // check if the element is another obj, an array, or a string
       switch (Object.getPrototypeOf(element)) {
         case Object.prototype:
+
           // iterate through the object's properties
           iterate(element);
+
+          console.log(element);
           break;
         case Array.prototype:
           // convert the element from object to array then loop through the array's objects
           let array = Array.from(element);
-          console.log("start array!");
-          array.forEach(arrItem => {
-            // check if the array is storing an obj
-            if (isObject(arrItem)) {
-              iterate(arrItem);
-            } else {
-              // else just print the array item
-              console.log(arrItem);
-            }
-          });
-          console.log("end array!");
+          // first, check if array is NOT empty
+          if (!array.length == 0) {
+            array.forEach(arrItem => {
+              // check if the array is storing an obj
+              if (isObject(arrItem)) {
+                iterate(arrItem);
+              } else {
+                // else just print the array item
+                console.log(arrItem);
+              }
+            });
+          }
           break;
         default:
-          console.log(element);
+          console.log(key + ": " + element);
           break;
       }
     }
@@ -68,6 +83,7 @@ function displayData(obj) {
 // run this function every time the button is pressed
 function generateSpell() {
   getContent().then(data => {
+    clear();
     displayData(data);
   }).catch(err => {
     console.error(err);
@@ -100,11 +116,19 @@ function iterate(obj) {
     if (obj.hasOwnProperty(key)) {
       let value = obj[key];
 
+      // if the value is another object, keep looping through.
+      // otherwise, just return the key AND value
       if (isObject(value)) {
         iterate(value);
       } else {
-        console.log('Key: ' + key +  ', Value:' + value);
+        return value;
       }
     }
   }
 };
+
+// we need to clear all data to grab a fresh spell
+function clear() {
+  const body = document.querySelector("#spellCard");
+  return body.innerHTML = "";
+}
